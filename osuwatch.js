f6255f657b26c6d5979428852606d4bdf7c35614
Adri- <adrien.boitelle@gmail.com>
2020-05-28 12:11:41 +0200

    commands

--- osuwatch.js
+++ osuwatch.js
@@ -1,6 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('config');
+const readline = require('readline');
+
+let running = true;
+
+const userconsole = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 if(!config.has("Discord.token")) {
   throw("Please use auth.json.example in config/ to fill in the bot token in auth.json");
@@ -12,8 +20,31 @@ if(token == "BOT_TOKEN") {
   throw("Please configure the discord bot token in 'config/auth.json'");
 }
 
+const usercommand = (command) => {
+  switch(command) {
+    case "help":
+      console.log("Commands are not yet implemented, except for 'exit'.");
+      break;
+    case "exit":
+      running = false;
+      client.destroy();
+      break;
+    default:
+      console.log("Unknown command. Type 'help' for a list of commands.");
+  }
+
+  if(running) {
+    userconsole.question('> ', usercommand);
+  } else {
+    console.log("Goodbye!");
+    process.exit();
+  }
+}
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  userconsole.question('> ', usercommand);
 });
 
 client.on('message', msg => {
